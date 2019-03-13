be3f1ba2e63772d94394924ec63875170c7fe007
uenz98 <marco.penzo98@gmail.com>
2019-03-13 11:50:56 +0100

    created menu list

--- PortaleVideogiochi/src/app/menu/menu.component.ts
+++ PortaleVideogiochi/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MenuItem } from 'src/app/model/menu-item';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  menuList: MenuItem[] = [
+    {id: 1, descrizione: 'Home', selezionato: false},
+    {id: 2, descrizione: 'Lista', selezionato: false},
+    {id: 3, descrizione: 'Modifica', selezionato: false}
+  ];
+
+  @Output('showSection')
+  showSectionEvent: EventEmitter<number> = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showSection(id: number){
+    this.showSectionEvent.emit(id);
   }
 
 }

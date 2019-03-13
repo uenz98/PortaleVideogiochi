47926aabfaf6fd69f6a543fda5dbfb001a707751
uenz98 <marco.penzo98@gmail.com>
2019-03-13 12:25:38 +0100

    gameListCreation

--- PortaleVideogiochi/src/app/components/game-list/game-list.component.ts
+++ PortaleVideogiochi/src/app/components/game-list/game-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MenuItem } from 'src/app/model/menu-item';
 
 @Component({
   selector: 'app-game-list',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameListComponent implements OnInit {
 
+  gameList: MenuItem[] = [
+    {id:1, descrizione:"Counter Strike: Global Offensive", selezionato: false},
+    {id:2, descrizione:"Grand Theft Auto 5", selezionato: false},
+    {id:3, descrizione:"Minecraft", selezionato: false},
+    {id:4, descrizione:"The Elder Scrolls V: Skyrim", selezionato: false},
+    {id:5, descrizione:"Keep Talking and Nobody Explodes", selezionato: false},
+    {id:6, descrizione:"League of Legends", selezionato: false},
+    {id:7, descrizione:"Fallout 4", selezionato: false},
+  ]
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  getGameList(){
+    return this.gameList;
+  }
+
+
+  @Output('showDetail')
+  showDetailEvent: EventEmitter<number> = new EventEmitter();
+
+  showDetail(id:number){
+    this.showDetailEvent.emit(id);
+  }
 }
